51401f7643a625ca2dd5072c33a3db58174e3781
Elina <elinamuhamedova13@gmail.com>
2024-10-01 19:54:23 +0300

    fixed the constructor & predict method, checked on input points

--- Computational Mathematics/Interpolation/main.cpp
+++ Computational Mathematics/Interpolation/main.cpp
@@ -1,13 +1,45 @@
 #include "newton_polynomial.hpp"
+#include <iostream>
+#include <cmath>
 
 int main() {
-    std::array<double, 3> points{};
-    int N = points.size();
+    std::array<std::array<double, 2>, 6> endpoints_choice = {{ {0, 2}, {0, 1}, {0, 0.5}, {0, 0.25}, {0, 0.125}, {0, 0.0625} }};
 
-    std::array<double, 2> endpoints = {0, 2};
-    double delta = (endpoints[1] - endpoints[0]) / (N - 1);
-    for (int i = 1; i < N; ++i)
-        points[i] += i * delta;
+    std::array<double, 3> even_points;
+    int N = even_points.size();
+    std::array<double, 3> values;
+    for (std::array<double, 2> endpoints : endpoints_choice) {
+        double delta = (endpoints[1] - endpoints[0]) / (N - 1);
+        for (int i = 0; i < N; ++i) {
+            double node = i * delta;
+            even_points[i] = node;
+            values[i] = std::exp(node);
+        }
+        NewtonInterpolant D = NewtonInterpolant<double, double, 3>{even_points, values};
+        std::array<double, 100> x_plot;
+        std::array<double, 100> y_plot;
+        delta = (endpoints[1] - endpoints[0]) / 99;
+        for (int i = 0; i < 100; ++i) {
+            double node = i * delta;
+            x_plot[i] = node;
+            y_plot[i] = D.predict(node);
+        }
+        std::cout << "Input data" << std::endl;
+        for (int i = 0; i < N; ++i) {
+            std::cout << "  x" + std::to_string(i) + " = " << even_points[i]
+            << "    f(x" + std::to_string(i) + ") = " << values[i] << std::endl;
+        }
+        //std::cout << "Interpolated data" << std::endl;
+        //for (int i = 0; i < 100; ++i) {
+        //    std::cout << "  x" + std::to_string(i) + " = " << x_plot[i]
+        //    << "    y(x" + std::to_string(i) + ") = " << y_plot[i] << std::endl;
+        //}
+        //std::cout << std::endl;
+        for (int i = 0; i < N; ++i) {
+            std::cout << "  x" + std::to_string(i) + " = " << even_points[i]
+            << "    y(x" + std::to_string(i) + ") = " << D.predict(even_points[i]) << std::endl;
+        }
+    }
 
     return 0;
 }
--- Computational Mathematics/Interpolation/newton_polynomial.hpp
+++ Computational Mathematics/Interpolation/newton_polynomial.hpp
@@ -4,21 +4,32 @@
 template<typename xType, typename yType, unsigned int N>
 class NewtonInterpolant {
 
-    std::array<yType, N> alpha_;
     std::array<xType, N> points_;
+    std::array<yType, N> alpha_;
 
     public:
     NewtonInterpolant(const std::array<xType, N>& points, const std::array<yType, N>& values) : points_(points){
         std::array<yType, N> alpha = values;
-        for (int i = 1; i < N; ++i) 
-            if (points_[i] - points_[0] != 0)
-                alpha[i] = (alpha[i] - alpha[i - 1]) / (points[i] - points[0]);
+        for (int i = 0; i < N; ++i) {
+            for (int j = N - 1; j > i; --j)
+                alpha[j] = (alpha[j] - alpha[j - 1]) / (points_[j] - points_[j - i - 1]);
+        }
         alpha_ = alpha;
     }
     yType predict(const xType& x) const {
         yType ans = alpha_[N - 1];
-        for (int i = N - 1; i > 0; --i)
-            ans = ans * (x - points_[i]) + alpha_[i - 1];
+        for (int i = N - 1; i > 0; --i) {
+            ans = ans * (x - points_[i - 1]) + alpha_[i - 1];
+        }
         return ans;
     }
+};
+
+template<typename xType, typename yType, unsigned int N>
+class Plot {
+
+    std::array<xType, N> x_data;
+    std::array<yType, N> y_data;
+    
+
 };

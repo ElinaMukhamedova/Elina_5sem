3983b608ca0a08b66fdbdcdfd3765fb7ccc9a99e
Elina <elinamuhamedova13@gmail.com>
2024-10-06 11:25:46 +0300

    add structure to third_party

--- Celestial mechanics/time/src/Time.cpp
+++ Celestial mechanics/time/src/Time.cpp
@@ -9,3 +9,14 @@ Time::Time(double jd1, double jd2) {
     jdInt_ = jd1_int + jd2_int;
     jdFrac_ = jd1_frac + jd2_frac;
 }
+
+Time Time::fromJD(double jd) noexcept {
+    double jd_int;
+    double jd_frac = std::modf(jd, &jd_int);
+    return Time(jd_int, jd_frac);
+}
+
+Time Time::fromMJD(double mjd) noexcept {
+    return fromJD(mjd + 2400000.5);
+}
+

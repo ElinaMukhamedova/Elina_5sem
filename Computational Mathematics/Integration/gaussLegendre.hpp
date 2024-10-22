a9a266ff5e39df1339631d72cb0aad3ad13ef470
Elina <elinamuhamedova13@gmail.com>
2024-10-22 08:47:32 +0300

    move N to template parameter

--- Computational Mathematics/Integration/gaussLegendre.hpp
+++ Computational Mathematics/Integration/gaussLegendre.hpp
@@ -16,14 +16,13 @@ using DifType = decltype(std::declval<T>() - std::declval<T>());    // DifType<T
 template <typename T>
 using DerivativeType = decltype(1.0 / (std::declval<T>() - std::declval<T>()));
 
-template<typename T>
+template<typename T, std::size_t N>
 class LegendrePolynomial {
 private:
     const T start_;
     const T end_;
-    std::size_t N_;
-    std::array<double, N_ + 1> weights_;
-    std::array<T, N_ + 1> roots_;
+    std::array<double, N> weights_;
+    std::array<T, N> roots_;
 
     struct Result {
         double value_;
@@ -37,7 +36,7 @@ private:
 
         T value_minus_1 = 1;
         const T k = 1 / (x * x - 1);
-        for(std::size_t step = 2; step <= N_; step++) {
+        for(std::size_t step = 2; step <= N; step++) {
             double value = ((2 * step - 1) * x * result.value_ - (step - 1) * value_minus_1) / step;
             result.derivative_ = step * k * (x * value - result.value_);
 
@@ -49,8 +48,8 @@ private:
     }
 
     void calculateWeightAndRoot() {
-        for(std::size_t step = 0; step <= N_; step++) {
-            T root = std::cos(M_PI * (step - 0.25) / (N_ + 0.5));           // Tricomi approximation
+        for(std::size_t step = 0; step <= N; step++) {
+            T root = std::cos(M_PI * (step - 0.25) / (N + 0.5));           // Tricomi approximation
             Result result = calculatePolynomialValueAndDerivative(root);
 
             T ratio;
@@ -66,16 +65,16 @@ private:
     }
 
 public:
-    LegendrePolynomial(T start, T end, std::size_t N)
-        : start_(start), end_(end), N_(N), weights_(N + 1), roots_(N + 1) {
+    LegendrePolynomial(T start, T end)
+        : start_(start), end_(end),  weights_(N + 1), roots_(N + 1) {
         calculateWeightAndRoot();
     }
 
-    const std::array<double, N_ + 1> getWeights() const {
+    const std::array<double, N + 1> getWeights() const {
         return weights_;
     }
 
-    const std::array<T, N_ + 1> getRoots() const {
+    const std::array<T, N + 1> getRoots() const {
         return roots_;
     }
 
@@ -87,7 +86,7 @@ decltype(auto) integrate(
     const typename ArgumentGetter<Callable>::Argument& start,
     const typename ArgumentGetter<Callable>::Argument& end
                         ){
-                            const LegendrePolynomial legendrePolynomial(start, end, N);
+                            const LegendrePolynomial<typename ArgumentGetter<Callable>::Argument, N> legendrePolynomial(start, end);
                             const std::array<double, N + 1> & weights = legendrePolynomial.getWeights();
                             const std::array<typename ArgumentGetter<Callable>::Argument, N + 1> & roots = legendrePolynomial.getRoots();
 

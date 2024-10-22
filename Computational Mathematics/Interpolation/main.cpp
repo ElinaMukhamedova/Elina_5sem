a7f6d205bb265e91572914c2b24ccde72d9d2b9a
Elina <elinamuhamedova13@gmail.com>
2024-10-22 13:36:06 +0300

    graphs for evenly distributed nodes

--- Computational Mathematics/Interpolation/main.cpp
+++ Computational Mathematics/Interpolation/main.cpp
@@ -6,9 +6,11 @@ int main() {
     std::array<std::array<double, 2>, 6> endpoints_choice = {{ {0, 2}, {0, 1}, {0, 0.5}, {0, 0.25}, {0, 0.125}, {0, 0.0625} }};
 
     std::array<double, 3> even_points;
-    int N = even_points.size();
+    int N = 3;
     std::array<double, 3> values;
-    for (std::array<double, 2> endpoints : endpoints_choice) {
+    std::array<double, 6> errors_for_3_nodes; 
+    for (int range_i = 0; range_i < 6; ++range_i) {
+        std::array<double, 2> endpoints = endpoints_choice[range_i];
         double delta = (endpoints[1] - endpoints[0]) / (N - 1);
         for (int i = 0; i < N; ++i) {
             double node = i * delta;
@@ -16,26 +18,105 @@ int main() {
             values[i] = std::exp(node);
         }
         NewtonInterpolant<double, double, 3> D = NewtonInterpolant<double, double, 3>{even_points, values};
-        std::array<double, 100> x_plot;
-        std::array<double, 100> y_plot;
-        delta = (endpoints[1] - endpoints[0]) / 99;
-        for (int i = 0; i < 100; ++i) {
+        std::array<double, 1000> x_plot;
+        std::array<double, 1000> y_plot;
+        double max_error = 0;
+        delta = (endpoints[1] - endpoints[0]) / 999;
+        for (int i = 0; i < 1000; ++i) {
             double node = i * delta;
             x_plot[i] = node;
-            y_plot[i] = D.predict(node);
+            double y_predict = D.predict(node);
+            y_plot[i] = y_predict;
+            double error = std::abs(y_predict - std::exp(node));
+            if (error > max_error)
+                max_error = error;
         }
-        std::cout << "Input data" << std::endl;
-        for (int i = 0; i < N; ++i) {
-            std::cout << "  x" + std::to_string(i) + " = " << even_points[i]
-            << "    f(x" + std::to_string(i) + ") = " << values[i] << std::endl;
-        }
-        std::cout << "Interpolated data" << std::endl;
-        for (int i = 0; i < 100; ++i) {
-            std::cout << "  x" + std::to_string(i) + " = " << x_plot[i]
-            << "    y(x" + std::to_string(i) + ") = " << y_plot[i] << std::endl;
-        }
-        std::cout << std::endl;
+        errors_for_3_nodes[range_i] = max_error;
+        //std::cout << "Input data" << std::endl;
+        //for (int i = 0; i < N; ++i) {
+        //    std::cout << "  x" + std::to_string(i) + " = " << even_points[i]
+        //    << "    f(x" + std::to_string(i) + ") = " << values[i] << std::endl;
+        //}
+        //std::cout << "Interpolated data" << std::endl;
+        //for (int i = 0; i < 1000; ++i) {
+        //    std::cout << "  x" + std::to_string(i) + " = " << x_plot[i]
+        //    << "    y(x" + std::to_string(i) + ") = " << y_plot[i] << std::endl;
+        //}
+        //std::cout << std::endl;
+        //for (int i = 0; i < 1000; ++i)
+        //    std::cout << x_plot[i] << ", ";
+        //std::cout << std::endl;
+        //for (int i = 0; i < 1000; ++i)
+        //    std::cout << y_plot[i] << ", ";
+        //std::cout << std::endl;
     }
+    for (int range_i = 0; range_i < 6; ++range_i)
+        std::cout << errors_for_3_nodes[range_i] << ", ";
+    std::cout << std::endl;
+
+    std::array<double, 4> even_points_4;
+    N = 4;
+    std::array<double, 4> values_4;
+    std::array<double, 6> errors_for_4_nodes; 
+    for (int range_i = 0; range_i < 6; ++range_i) {
+        std::array<double, 2> endpoints = endpoints_choice[range_i];
+        double delta = (endpoints[1] - endpoints[0]) / (N - 1);
+        for (int i = 0; i < N; ++i) {
+            double node = i * delta;
+            even_points_4[i] = node;
+            values_4[i] = std::exp(node);
+        }
+        NewtonInterpolant<double, double, 4> D_4 = NewtonInterpolant<double, double, 4>{even_points_4, values_4};
+        std::array<double, 1000> x_plot;
+        std::array<double, 1000> y_plot;
+        double max_error = 0;
+        delta = (endpoints[1] - endpoints[0]) / 999;
+        for (int i = 0; i < 1000; ++i) {
+            double node = i * delta;
+            x_plot[i] = node;
+            double y_predict = D_4.predict(node);
+            y_plot[i] = y_predict;
+            double error = std::abs(y_predict - std::exp(node));
+            if (error > max_error)
+                max_error = error;
+        }
+        errors_for_4_nodes[range_i] = max_error;
+    }
+    for (int range_i = 0; range_i < 6; ++range_i)
+        std::cout << errors_for_4_nodes[range_i] << ", ";
+    std::cout << std::endl;
+
+    std::array<double, 5> even_points_5;
+    N = 5;
+    std::array<double, 5> values_5;
+    std::array<double, 6> errors_for_5_nodes; 
+    for (int range_i = 0; range_i < 6; ++range_i) {
+        std::array<double, 2> endpoints = endpoints_choice[range_i];
+        double delta = (endpoints[1] - endpoints[0]) / (N - 1);
+        for (int i = 0; i < N; ++i) {
+            double node = i * delta;
+            even_points_5[i] = node;
+            values_5[i] = std::exp(node);
+        }
+        NewtonInterpolant<double, double, 5> D_5 = NewtonInterpolant<double, double, 5>{even_points_5, values_5};
+        std::array<double, 1000> x_plot;
+        std::array<double, 1000> y_plot;
+        double max_error = 0;
+        delta = (endpoints[1] - endpoints[0]) / 999;
+        for (int i = 0; i < 1000; ++i) {
+            double node = i * delta;
+            x_plot[i] = node;
+            double y_predict = D_5.predict(node);
+            y_plot[i] = y_predict;
+            double error = std::abs(y_predict - std::exp(node));
+            if (error > max_error)
+                max_error = error;
+        }
+        errors_for_5_nodes[range_i] = max_error;
+    }
+    for (int range_i = 0; range_i < 6; ++range_i)
+        std::cout << errors_for_5_nodes[range_i] << ", ";
+    std::cout << std::endl;
 
     return 0;
 }

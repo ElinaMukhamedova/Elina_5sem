a481f137426cb8decde8335880be413ff57bdcf7
Elina <elinamuhamedova13@gmail.com>
2024-10-22 20:11:56 +0300

    integration for N = 3 and N = 4

--- Computational Mathematics/Integration/gaussLegendre.hpp
+++ Computational Mathematics/Integration/gaussLegendre.hpp
@@ -1,6 +1,7 @@
 #include <array>
 #include <type_traits>
 #include <cmath>
+#include <iostream>
 
 template <typename A>
 struct ArgumentGetter;
@@ -21,8 +22,8 @@ class LegendrePolynomial {
 private:
     const T start_;
     const T end_;
-    std::array<double, N> weights_;
-    std::array<T, N> roots_;
+    std::array<double, N + 1> weights_;
+    std::array<T, N + 1> roots_;
 
     struct Result {
         double value_;
@@ -36,7 +37,7 @@ private:
 
         T value_minus_1 = 1;
         const T k = 1 / (x * x - 1);
-        for(std::size_t step = 2; step <= N; step++) {
+        for(std::size_t step = 2; step <= N; ++step) {
             double value = ((2 * step - 1) * x * result.value_ - (step - 1) * value_minus_1) / step;
             result.derivative_ = step * k * (x * value - result.value_);
 
@@ -48,7 +49,7 @@ private:
     }
 
     void calculateWeightAndRoot() {
-        for(std::size_t step = 0; step <= N; step++) {
+        for(std::size_t step = 0; step <= N; ++step) {
             T root = std::cos(M_PI * (step - 0.25) / (N + 0.5));           // Tricomi approximation
             Result result = calculatePolynomialValueAndDerivative(root);
 
@@ -57,7 +58,7 @@ private:
                 ratio = result.value_ / result.derivative_;
                 root -= ratio;
                 result = calculatePolynomialValueAndDerivative(root);
-            } while (std::abs(ratio) > 0);
+            } while (std::abs(ratio) > 1e-15);
 
             roots_[step] = root;
             weights_[step] = 2.0 / ((1 - root * root) * result.derivative_ * result.derivative_);
@@ -65,8 +66,7 @@ private:
     }
 
 public:
-    LegendrePolynomial(T start, T end)
-        : start_(start), end_(end),  weights_(N + 1), roots_(N + 1) {
+    LegendrePolynomial(T start, T end) : start_(start), end_(end) {
         calculateWeightAndRoot();
     }
 
@@ -81,7 +81,7 @@ public:
 };
 
 template <std::size_t N, typename Callable>
-decltype(auto) integrate(
+double integrate(
     const Callable& func,
     const typename ArgumentGetter<Callable>::Argument& start,
     const typename ArgumentGetter<Callable>::Argument& end
@@ -93,7 +93,7 @@ decltype(auto) integrate(
                             const typename ArgumentGetter<Callable>::Argument half_width = 0.5 * (end - start);
                             const typename ArgumentGetter<Callable>::Argument centre_point = 0.5 * (start + end);
 
-                            decltype(auto) integral = 0;
+                            double integral = 0;
                             for(int step = 1; step <= N; step++) {
                                 integral += weights[step] * func(half_width * roots[step] + centre_point);
                             }
@@ -101,12 +101,34 @@ decltype(auto) integrate(
                             return integral * half_width;
                         }
 
-//template <typename Callable, std::size_t N>
-//decltype(auto) integrate(
-//    const Callable& func,
-//    const typename ArgumentGetter<Callable>::Argument& start,
-//    const typename ArgumentGetter<Callable>::Argument& end,
-//    const DifType<typename ArgumentGetter<Callable>::Argument>& dx
-//                        ){
-//
-//                        }
+template <std::size_t N, typename Callable>
+double integrate(
+    const Callable& func,
+    const typename ArgumentGetter<Callable>::Argument& a,
+    const typename ArgumentGetter<Callable>::Argument& b,
+    const DifType <typename ArgumentGetter<Callable>::Argument>& dx
+                        ){
+                            int n = (int) ((b - a) / dx);
+                            double integral = 0;
+                            for (int i = 0; i < n; ++i) {
+                                typename ArgumentGetter<Callable>::Argument start = i * dx;
+                                typename ArgumentGetter<Callable>::Argument end = start + dx;
+                                const LegendrePolynomial<typename ArgumentGetter<Callable>::Argument, N> legendrePolynomial(start, end);
+                                const std::array<double, N + 1> & weights = legendrePolynomial.getWeights();
+                                const std::array<typename ArgumentGetter<Callable>::Argument, N + 1> & roots = legendrePolynomial.getRoots();
+    
+                                const typename ArgumentGetter<Callable>::Argument half_width = 0.5 * (end - start);
+                                const typename ArgumentGetter<Callable>::Argument centre_point = 0.5 * (start + end);
+    
+                                double integral_i = 0;
+                                for(int step = 1; step <= N; step++) {
+                                    integral_i += weights[step] * func(half_width * roots[step] + centre_point);
+                                }
+                                integral_i *= half_width;
+
+                                integral += integral_i;
+                            }
+
+
+                            return integral;
+                        }
--- Computational Mathematics/Integration/main.cpp
+++ Computational Mathematics/Integration/main.cpp
@@ -7,9 +7,49 @@ double s(const double x) {return std::sin(x);}
 
 int main() {
     const auto sin =[](const double x) {return std::sin(x);};
-    //std::cout << f_sin(M_PI / 2) << std::endl;
-    decltype(auto) integral = integrate<13>(s, 0.0, 10.0);
+    decltype(auto) integral_30 = integrate<30>(s, 0.0, 10.0);
+    std::cout << integral_30 << std::endl;
+
+    decltype(auto) integral_1 = integrate<1>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_1) << std::endl;
+
+    decltype(auto) integral_2 = integrate<2>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_2) << std::endl;
+
+    decltype(auto) integral_3 = integrate<3>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_3) << std::endl;
+
+    decltype(auto) integral_4 = integrate<4>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_4) << std::endl;
+
+    decltype(auto) integral_5 = integrate<5>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_5) << std::endl;
+
+    decltype(auto) integral_6 = integrate<6>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_6) << std::endl;
+
+    decltype(auto) integral_7 = integrate<7>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_7) << std::endl;
+
+    decltype(auto) integral_8 = integrate<8>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_8) << std::endl;
+
+    decltype(auto) integral_9 = integrate<9>(s, 0.0, 10.0);
+    std::cout << std::abs(integral_30 - integral_9) << std::endl;
+
+    decltype(auto) integral = integrate<4>(s, 0.0, 10.0, 10.0);
     std::cout << integral << std::endl;
+
+    for (int i = 1; i <= 10; ++i){
+        decltype(auto) integral = integrate<4>(s, 0.0, 10.0, (double)(10.0 / i));
+        std::cout << std::abs(integral_30 - integral) << std::endl;
+        //std::cout << (double)(10.0 / i) << std::endl;
+    }
+    for (int i = 1; i <= 10; ++i){
+        decltype(auto) integral = integrate<3>(s, 0.0, 10.0, (double)(10.0 / i));
+        std::cout << std::abs(integral_30 - integral) << std::endl;
+        //std::cout << (double)(10.0 / i) << std::endl;
+    }
 
     return 0;
 }

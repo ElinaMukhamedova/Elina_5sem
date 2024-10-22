8caf92eace583c80baf8aa156d47309338e79a33
Elina <elinamuhamedova13@gmail.com>
2024-10-22 11:31:28 +0300

    error

--- Computational Mathematics/Integration/main.cpp
+++ Computational Mathematics/Integration/main.cpp
@@ -1,9 +1,11 @@
 #include "gaussLegendre.hpp"
 #include <cmath>
 #include <iostream>
+#include <math.h>
 
 int main() {
-    std::function<double(double)> f_sin = (double(*)(double))& std::sin;
+    const std::function<double(double)>& f_sin = (double(*)(double)) std::sin;
+    //std::cout << f_sin(M_PI / 2) << std::endl;
     decltype(auto) integral = integrate<std::function<double(double)>, 13>(f_sin, 0.0, 10.0);
     std::cout << integral << std::endl;
 

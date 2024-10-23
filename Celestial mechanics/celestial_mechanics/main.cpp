169228162a146649aaede1eeb54784ba724ce4ed
Elina <elinamuhamedova13@gmail.com>
2024-10-23 08:12:17 +0300

    some fixes

--- Celestial mechanics/celestial_mechanics/main.cpp
+++ Celestial mechanics/celestial_mechanics/main.cpp
@@ -1,9 +1,9 @@
 #include <iostream>
-#include <time/Time.hpp>
+#include "time/Time.hpp"
 
 int main() {
     Time today = Time::fromCalendar(2024, 10, 7, 20, 49, 13);
-    std::cout << today.jdInt() << std::endl;
+    std::cout << today.jdFrac() << std::endl;
 
     return 0;
 }
--- Celestial mechanics/celestial_mechanics/tests/test_1.cpp
+++ Celestial mechanics/celestial_mechanics/tests/test_1.cpp
@@ -1,2 +1,2 @@
 #include <gtest/gtest.h>
-#include "Time.hpp"
+#include "time/Time.hpp"

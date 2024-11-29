e511f68133bfb9af66dd46d07dbc49273f5e3815
Elina <elinamuhamedova13@gmail.com>
2024-11-29 08:23:56 +0300

    GCRS2ITRS

--- Celestial mechanics/celestial_mechanics/main.cpp
+++ Celestial mechanics/celestial_mechanics/main.cpp
@@ -14,45 +14,51 @@ int main() {
     Eigen::Vector<double, 3> r_GCRS{6700e3, 0, 0};
     
     const auto tt = Time<Scale::TT>::fromCalendar(2019, 9, 13, 12, 45, 0);
-    std::cout << "tt.jdInt() = " << std::setprecision(15) <<  tt.jdInt() << std::endl;
-    std::cout << "tt.jdFrac() = " << std::setprecision(15) <<  tt.jdFrac() << std::endl;
+    std::cout << "tt.jdInt() = " << std::setprecision(17) <<  tt.jdInt() << std::endl;
+    std::cout << "tt.jdFrac() = " << std::setprecision(17) <<  tt.jdFrac() << std::endl;
     
     const auto utc = coordinatesConverter.timeConverter().convert<Scale::UTC>(tt);
-    std::cout << "utc.jdInt() = " << std::setprecision(15) <<  utc.jdInt() << std::endl;
-    std::cout << "utc.jdFrac() = " << std::setprecision(15) <<  utc.jdFrac() << std::endl;
+    std::cout << "utc.jdInt() = " << std::setprecision(17) <<  utc.jdInt() << std::endl;
+    std::cout << "utc.jdFrac() = " << std::setprecision(17) <<  utc.jdFrac() << std::endl;
 
     const auto ut1 = coordinatesConverter.timeConverter().convert<Scale::UT1>(tt);
-    std::cout << "ut1.jdInt() = " << std::setprecision(15) <<  ut1.jdInt() << std::endl;
-    std::cout << "ut1.jdFrac() = " << std::setprecision(15) <<  ut1.jdFrac() << std::endl;
+    std::cout << "ut1.jdInt() = " << std::setprecision(17) <<  ut1.jdInt() << std::endl;
+    std::cout << "ut1.jdFrac() = " << std::setprecision(17) <<  ut1.jdFrac() << std::endl;
 
-    std::cout << std::setprecision(15) << "dX = " << EOPcontainer.dX(utc.mjd()) << std::endl;
-    std::cout << std::setprecision(15) << "dY = " << EOPcontainer.dY(utc.mjd()) << std::endl;
-    std::cout << std::setprecision(15) << "xp = " << EOPcontainer.xTerr(utc.mjd()) << std::endl;
-    std::cout << std::setprecision(15) << "yp = " << EOPcontainer.yTerr(utc.mjd()) << std::endl;
+    std::cout << std::setprecision(17) << "dX = " << EOPcontainer.dX(utc.mjd()) << std::endl;
+    std::cout << std::setprecision(17) << "dY = " << EOPcontainer.dY(utc.mjd()) << std::endl;
+    std::cout << std::setprecision(17) << "xp = " << EOPcontainer.xTerr(utc.mjd()) << std::endl;
+    std::cout << std::setprecision(17) << "yp = " << EOPcontainer.yTerr(utc.mjd()) << std::endl;
 
     std::cout << std::endl;
 
     Eigen::Matrix<double, 3, 3> gcrs2cirs = coordinatesConverter.GCRStoCIRS(utc);
     std::cout << "GCRS to CIRS (precession & nutation) matrix:" << std::endl;
-    std::cout << std::setprecision(15) << gcrs2cirs << std::endl;
+    std::cout << std::setprecision(17) << gcrs2cirs << std::endl;
 
     std::cout << std::endl;
 
     Eigen::Matrix<double, 3, 3> cirs2tirs = coordinatesConverter.rotateCIRStoTIRS(utc).toRotationMatrix();
     std::cout << "CIRS to TIRS (intrinsic rotation) matrix:" << std::endl;
-    std::cout << std::setprecision(15) << cirs2tirs << std::endl;
+    std::cout << std::setprecision(17) << cirs2tirs << std::endl;
 
     std::cout << std::endl;
 
     Eigen::Matrix<double, 3, 3> tirs2itrs = coordinatesConverter.PolarMotion(utc).toRotationMatrix();
     std::cout << "TIRS to ITRS (polar motion) matrix:" << std::endl;
-    std::cout << std::setprecision(15) << tirs2itrs << std::endl;
+    std::cout << std::setprecision(17) << tirs2itrs << std::endl;
 
     std::cout << std::endl;
 
     Eigen::Matrix<double, 3, 3> gcrs2itrs = coordinatesConverter.GCRStoITRS(utc);
     std::cout << "GCRS to ITRS matrix:" << std::endl;
-    std::cout << std::setprecision(15) << gcrs2cirs << std::endl;
+    std::cout << std::setprecision(17) << gcrs2cirs << std::endl;
+
+    std::cout << std::endl;
+
+    Eigen::Matrix<double, 3, 3> result = coordinatesConverter.GCRS2ITRS(utc);
+    std::cout << "rc2t matrix:" << std::endl;
+    std::cout << std::setprecision(17) << result << std::endl;
 
     return 0;
 }
--- Celestial mechanics/celestial_mechanics/src/celestial_mechanics/coordinates/ReferenceSystemConverter.hpp
+++ Celestial mechanics/celestial_mechanics/src/celestial_mechanics/coordinates/ReferenceSystemConverter.hpp
@@ -69,4 +69,32 @@ class ReferenceSystemConverter {
             Eigen::Quaternion<double> itrs2gcrs = GCRStoTIRS(utc).conjugate() * PolarMotion(utc).conjugate();
             return itrs2gcrs.toRotationMatrix();
         }
+
+        Eigen::Matrix<double, 3, 3> GCRS2ITRS(Time<Scale::UTC> utc) {
+            double utcMJD = utc.mjd();
+
+            const auto ut1 = timeConverter_.convert<Scale::UT1>(utc);
+            double era = iauEra00(ut1.jdInt(), ut1.jdFrac());
+
+            const auto tt = timeConverter_.convert<Scale::TT>(utc);
+            double tt1 = tt.jdInt(); double tt2 = tt.jdFrac();
+            double dX = EOPcontainer_.dX(utcMJD); double dY = EOPcontainer_.dY(utcMJD);
+            double X, Y;
+            iauXy06(tt1, tt2, &X, &Y);
+            double s = iauS06(tt1, tt2, X, Y);
+            double rc2i[3][3];
+            iauC2ixys(X+dX, Y+dY, s, rc2i);
+
+            double rpom[3][3];
+            double xTerr = EOPcontainer_.xTerr(utcMJD); double yTerr = EOPcontainer_.yTerr(utcMJD);
+            double sTerr = iauSp00(tt1, tt2);
+            iauPom00(xTerr, yTerr, sTerr, rpom);
+
+            double rc2t[3][3];
+            iauC2tcio(rc2i, era, rpom, rc2t);
+
+            return Eigen::Matrix<double, 3, 3> {{rc2t[0][0], rc2t[0][1], rc2t[0][2]},
+                                                {rc2t[1][0], rc2t[1][1], rc2t[1][2]},
+                                                {rc2t[2][0], rc2t[2][1], rc2t[2][2]}};
+        }
 };
